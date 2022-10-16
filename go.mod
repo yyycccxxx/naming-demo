444bbbee0af027e10cd3978a7932ef9e70ed82c6
Chenxu Yang <ychenxu@vmware.com>
2022-10-16 11:39:04 +0800

    v1.0.0 call CreateBabyName() string

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yyycccxxx/naming-demo
+
+go 1.18
+
+require github.com/yyycccxxx/baby-naming v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "github.com/yyycccxxx/baby-naming/naming"
+)
+
+func main() {
+  fmt.Println(naming.CreateBabyName())
+}
