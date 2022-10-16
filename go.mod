f4fac87ccb0a35bcbd8844c293ca01231c1d4e70
Chenxu Yang <ychenxu@vmware.com>
2022-10-16 17:47:52 +0800

    call the CreateBabyName() function by different strategies: subdir and branch

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/yyycccxxx/naming-demo
 
 go 1.18
 
-require github.com/yyycccxxx/baby-naming v1.0.0 // indirect
+require (
+	github.com/yyycccxxx/baby-naming v1.0.0 // indirect
+	github.com/yyycccxxx/baby-naming/v2 v2.0.0-20221016065953-751a3e2b971f // indirect
+	github.com/yyycccxxx/baby-naming/v3 v3.0.0-20221016073539-0978372c469b // indirect
+	github.com/yyycccxxx/baby-naming/v4 v4.0.0-20221016090301-655a965a7cf7 // indirect
+	github.com/yyycccxxx/baby-naming/v5 v5.0.0-20221016092103-ce9fb033d467 // indirect
+)
--- main.go
+++ main.go
@@ -3,8 +3,29 @@ package main
 import (
   "fmt"
   "github.com/yyycccxxx/baby-naming/naming"
+  naming_v2 "github.com/yyycccxxx/baby-naming/v2/naming"
+  naming_v3 "github.com/yyycccxxx/baby-naming/v3/naming"
+  naming_v4 "github.com/yyycccxxx/baby-naming/v4/naming"
+  naming_v5 "github.com/yyycccxxx/baby-naming/v5/naming"
 )
 
 func main() {
+  fmt.Println("\n-----------v1: CreateBabyName()----------------------------------------")
   fmt.Println(naming.CreateBabyName())
+
+  fmt.Println("\n-----------subDirectory v2: CreateBabyName(male bool)------------------")
+  fmt.Println("male: ", naming_v2.CreateBabyName(true))
+  fmt.Println("female: ", naming_v2.CreateBabyName(false))
+  
+  fmt.Println("\n-----------sub directory v3: CreateBabyName(male bool, minLen int)-----")
+  fmt.Println("male and minLen is 5: ", naming_v3.CreateBabyName(true, 5))
+  fmt.Println("female and minLen is 5: ", naming_v3.CreateBabyName(false, 5))
+  
+  fmt.Println("\n-----------branch v4: CreateBabyName(male bool)------------------------")
+  fmt.Println("male: ", naming_v4.CreateBabyName(true))
+  fmt.Println("female: ", naming_v4.CreateBabyName(false))
+  
+  fmt.Println("\n------------branch v5: CreateBabyName(male bool, minLen int)-----------")
+  fmt.Println("male and minLen is 3: ", naming_v5.CreateBabyName(true, 3))
+  fmt.Println("female and minLen is 3: ", naming_v5.CreateBabyName(false, 3))
 }
